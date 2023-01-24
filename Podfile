7e95d86f49fd6b083d45a2aa3d2729478f6ac1d1
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2023-01-24 21:02:31 +0200

    Prepare UI to change further

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '16.0'
+
+target 'SnapKitExample' do
+  pod 'SnapKit', '~> 5.6.0'
+
+end
--- SnapKitExample/ViewController.swift
+++ SnapKitExample/ViewController.swift
@@ -6,14 +6,109 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - View Properties
+    private let padding: CGFloat = 12
+    
+    private lazy var backButton: UIButton = {
+        var configuration = UIButton.Configuration.tinted()
+        configuration.title = "Back to Profile"
+        configuration.image = UIImage(systemName: "arrowshape.backward")
+        configuration.imagePadding = 4
+        
+        configuration.baseBackgroundColor = .systemBlue
+        configuration.baseForegroundColor = .systemBlue
+
+        let button = UIButton(configuration: configuration)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var mainLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.minimumScaleFactor = 0.7
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var newAddButton: UIButton = {
+        var configuration = UIButton.Configuration.tinted()
+        configuration.title = "Back to Profile"
+        configuration.image = UIImage(systemName: "plus.circle")
+        configuration.imagePadding = 4
+        
+        configuration.baseBackgroundColor = .systemRed
+        configuration.baseForegroundColor = .systemRed
+
+        let button = UIButton(configuration: configuration)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var segment: UISegmentedControl = {
+        let segment = UISegmentedControl(items: ["◀︎", "▶︎"])
+        segment.addTarget(self, action: #selector(segmentTap), for: .valueChanged)
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        return segment
+    }()
+    
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        updateUI()
     }
+    
+    // MARK: - Methods
+    
+    @objc private func segmentTap(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: print("short text")
+        case 1: print("wide text")
+        default: break
+        }
+    }
+    
+    private func updateUI() {
+        view.backgroundColor = .systemBackground
+        view.addAllSubbviews(backButton, segment, newAddButton, mainLabel)
+        
+        /// Back button
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(padding)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(padding)
+        }
+        
+        /// Segmented switch
+        segment.snp.makeConstraints { make in
+            make.trailing.equalTo(view.snp.trailing).offset(-padding)
+            make.centerY.equalTo(backButton.snp.centerY)
+            make.height.equalTo(backButton.snp.height)
+        }
+        
+        /// New add button
+        newAddButton.snp.makeConstraints { make in
+            make.trailing.equalTo(segment.snp.leading).offset(-padding)
+            make.centerY.equalTo(backButton.snp.centerY)
+        }
+        
+        /// Main label
+        mainLabel.snp.makeConstraints { make in
+            make.leading.equalTo(backButton.snp.trailing).offset(padding)
+            make.trailing.equalTo(newAddButton.snp.leading).offset(-padding)
+        }
+    }
+}
 
-
+private extension UIView {
+    func addAllSubbviews(_ views: UIView...) {
+        views.forEach { addSubview($0) }
+    }
 }
 
